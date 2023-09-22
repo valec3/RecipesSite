76f5f8f17d96bd564b4e58f1899d43eb149b37ff
Victor Raul <v1c70rotakmer@gmail.com>
2023-09-21 23:50:36 -0500

    Add styles to section tabs

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -34,12 +34,17 @@ const Tabs = () => {
                         <div className='ingredients'>
                             <ul>
                                 <li><GiCheckMark size="18px" color="#6fcb9f" />&nbsp;<span>Fresh ground pepper</span></li>
+                                <li><GiCheckMark size="18px" color="#6fcb9f" />&nbsp;<span>Fresh ground pepper</span></li>
+                                <li><GiCheckMark size="18px" color="#6fcb9f" />&nbsp;<span>Fresh ground pepper</span></li>
+                                <li><GiCheckMark size="18px" color="#6fcb9f" />&nbsp;<span>Fresh ground pepper</span></li>
+                                <li><GiCheckMark size="18px" color="#6fcb9f" />&nbsp;<span>Fresh ground pepper</span></li>
+                                <li><GiCheckMark size="18px" color="#6fcb9f" />&nbsp;<span>Fresh ground pepper</span></li>
                             </ul>
                         </div>
                     </div>
                     <div className="right-col">
                         <div className="image-wrapper">
-                        <img src="https://images.unsplash.com/photo-1607532941433-304659e8198a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1378&q=80" alt="" />
+                            <img src="https://images.unsplash.com/photo-1607532941433-304659e8198a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1378&q=80" alt="" />
                         </div>
                     </div>
             </div>
